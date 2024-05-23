ff0692c1293ed88f93db89c6e3db3cfd48af40f8
Tkayy729 <emmanueltweneboah71@gmail.com>
2024-05-23 15:04:13 +0200

    project setup with few start-up pages

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import "./App.css";
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
+import Home from "./pages/home";
+import About from "./pages/about";
+import Vans from "./pages/vans";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <header>
+        <Link className="site-logo" to="/">#VanLife</Link>
+        <nav>
+          <Link to="/about">About</Link>
+          <Link to="/vans">Vans</Link>
+        </nav>
+      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/vans" element={<Vans />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
