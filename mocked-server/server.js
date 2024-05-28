3093e183d974c3ac4a557c8ec26411283259eb4a
Tkayy729 <emmanueltweneboah71@gmail.com>
2024-05-28 17:55:38 +0200

    imported server file in the main app

--- mocked-server/server.js
+++ mocked-server/server.js
@@ -13,7 +13,7 @@ createServer({
       description:
         "The Modest Explorer is a van designed to get you out of the house and into nature. This beauty is equipped with solar panels, a composting toilet, a water tank and kitchenette. The idea is that you can pack up your home and escape for a weekend or even longer!",
       imageUrl:
-        "https://assets.scrimba.com/advan ced-react/react-router/modest-explorer.png",
+        "https://assets.scrimba.com/advanced-react/react-router/modest-explorer.png",
       type: "simple",
     });
     server.create("van", {
